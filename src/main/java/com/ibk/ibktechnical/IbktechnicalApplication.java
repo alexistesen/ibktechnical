126ecefd7aca13f467131492237a99d2c0bced21
Alexis <jose.alexis.tesen.paredes@everis.nttdata.com>
2024-11-14 11:46:48 -0500

    git commit init

---
+++ src/main/java/com/ibk/ibktechnical/IbktechnicalApplication.java
@@ -0,0 +1,13 @@
+package com.ibk.ibktechnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbktechnicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbktechnicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibk/ibktechnical/IbktechnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibk.ibktechnical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbktechnicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
